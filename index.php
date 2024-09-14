43faa0a4ae1a61330b2be316241d8b93943b08aa
calebSmithDS <109048857+calebSmithDS@users.noreply.github.com>
2024-09-14 09:33:18 +0930

    Index page
    Started basic structure of index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Digital Garden Home Page</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Caleb Smith" />
+</head>
+<body>
+    <h1>Caleb's Digital Garden</h1>
+</body>
+
+</html>
